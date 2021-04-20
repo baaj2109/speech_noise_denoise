046b65aaae1e21ac494e7e31d5e6a9f5f8618028
kehwa <kehwaweng@staff.ipevo.com>
2021-04-20 09:06:03 +0800

    update run.py

--- run.py
+++ run.py
@@ -19,105 +19,135 @@ import time
 clean_sounds = glob.glob('/Volumes/IPEVO_X0244/speech_to_text/speech_to_text_dataset/facebookresearch/train/clean_trainset_wav/*')
 noisy_sounds = glob.glob('/Volumes/IPEVO_X0244/speech_to_text/speech_to_text_dataset/facebookresearch/train/noisy_trainset_wav/*')
 
-clean_sounds_list,_ = tf.audio.decode_wav(tf.io.read_file(clean_sounds[0]),desired_channels=1)
+clean_sounds_list, _ = tf.audio.decode_wav(tf.io.read_file(clean_sounds[0]), desired_channels = 1)
 for i in tqdm(clean_sounds[1:]):
-    so,_ = tf.audio.decode_wav(tf.io.read_file(i),desired_channels=1)
-    clean_sounds_list = tf.concat((clean_sounds_list,so),0)
+    so, _ = tf.audio.decode_wav(tf.io.read_file(i), desired_channels = 1)
+    clean_sounds_list = tf.concat((clean_sounds_list, so), 0)
 
-noisy_sounds_list,_ = tf.audio.decode_wav(tf.io.read_file(noisy_sounds[0]),desired_channels=1)
+noisy_sounds_list, _ = tf.audio.decode_wav(tf.io.read_file(noisy_sounds[0]), desired_channels = 1)
 for i in tqdm(noisy_sounds[1:]):
-    so,_ = tf.audio.decode_wav(tf.io.read_file(i),desired_channels=1)
-    noisy_sounds_list = tf.concat((noisy_sounds_list,so),0)
+    so, _ = tf.audio.decode_wav(tf.io.read_file(i), desired_channels = 1)
+    noisy_sounds_list = tf.concat((noisy_sounds_list, so), 0)
 
 print(f"clean sound list : {clean_sounds_list.shape}, noisy sounds list : {noisy_sounds_list.shape}")
+# clean_sounds_list = clean_sounds_list[: len(clean_sounds_list)]
+# noisy_sounds_list = noisy_sounds_list[: len(noisy_sounds_list)]
+
+clean_sounds_list = clean_sounds_list[: len(clean_sounds_list) // 2]
+noisy_sounds_list = noisy_sounds_list[: len(noisy_sounds_list) // 2]
 
 batching_size = 12000
-clean_train,noisy_train = [],[]
+clean_train,noisy_train = [], []
 
-for i in tqdm(range(0,clean_sounds_list.shape[0]-batching_size,batching_size)):
-    clean_train.append(clean_sounds_list[i:i+batching_size])
-    noisy_train.append(noisy_sounds_list[i:i+batching_size])
+#{##############################################################################
+urban_sound_list = glob.glob("/Volumes/IPEVO_X0244/speech_to_text/speech_to_text_dataset/UrbanSound8K/audio/fold*/*")
+concat_len = clean_sounds_list.shape[0]
+noise_concat = np.array([0]).reshape((1,1))
+while noise_concat.shape[0]  < concat_len + 1:
+    try:
+        file = np.random.choice(urban_sound_list)
+        wave, _ = tf.audio.decode_wav(tf.io.read_file(file), desired_channels = 1)
+        noise_concat = np.concatenate((noise_concat, wave))
+    except:
+        pass
+noise_concat = noise_concat[1: 1 + concat_len]
+
+clean_sounds_list = tf.concat((clean_sounds_list, clean_sounds_list), 0)
+noisy_sounds_list = tf.concat((noisy_sounds_list, noise_concat), 0)
+#}##############################################################################
+
+for i in tqdm(range(0, clean_sounds_list.shape[0] - batching_size, batching_size)):
+    clean_train.append(clean_sounds_list[i: i + batching_size])
+    noisy_train.append(noisy_sounds_list[i: i + batching_size])
 
 clean_train = tf.stack(clean_train)
 noisy_train = tf.stack(noisy_train)
 
 print(f"clean train shape: {clean_train.shape}")
 print(f"noise train shape: {noisy_train.shape}")
+del clean_sounds_list, noisy_sounds_list
 
 ################################################################################
 ## create a tf.data.Dataset
 ################################################################################
 
-def get_dataset(x_train,y_train):
-  dataset = tf.data.Dataset.from_tensor_slices((x_train,y_train))
-  dataset = dataset.shuffle(100).batch(64,drop_remainder=True)
-  return dataset
+def get_dataset(x_train, y_train):
+    dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    dataset = dataset.shuffle(100).batch(64, drop_remainder = True)
+    return dataset
 
-train_dataset = get_dataset(noisy_train[:40000],clean_train[:40000])
-test_dataset = get_dataset(noisy_train[40000:],clean_train[40000:])
+split_factor = 0.8
+split_size = int(noisy_train.shape[0] * split_factor)
+train_dataset = get_dataset(noisy_train[:split_size], clean_train[:split_size])
+test_dataset = get_dataset(noisy_train[split_size:], clean_train[split_size:])
+# train_dataset = get_dataset(noisy_train[:40000], clean_train[:40000])
+# test_dataset = get_dataset(noisy_train[40000:], clean_train[40000:])
+del clean_train, noisy_train
 
 ################################################################################
 ## create model
 ################################################################################
 
+SCALE = 2
+
 def build_model(input_size = 64000):
-    input_shape = layers.Input(shape=(input_size, 1))
+    input_shape = layers.Input(shape = (input_size, 1))
     x= input_shape
     if input_size == 4410:
-        paddings = tf.constant([[0,0],[0,6],[0,0]])        
+        paddings = tf.constant([[0, 0], [0, 6], [0, 0]])        
         x = tf.pad(x, paddings)
         
-    c1 = layers.Conv1D(filters = 2, 
+    c1 = layers.Conv1D(filters = 2 * SCALE, 
                        kernel_size = 32,
                        strides = 2,
                        padding = 'same',
                        activation = 'relu')(x)
-    c2 = layers.Conv1D(filters = 4,
+    c2 = layers.Conv1D(filters = 4 * SCALE,
                        kernel_size = 32,
                        strides = 2,
                        padding = 'same',
                        activation = 'relu')(c1)
-    c3 = layers.Conv1D(filters = 8,
+    c3 = layers.Conv1D(filters = 8 * SCALE,
                        kernel_size = 32,
                        strides = 2,
                        padding = 'same',
                        activation = 'relu')(c2)
-    c4 = layers.Conv1D(filters = 16,
+    c4 = layers.Conv1D(filters = 16 * SCALE,
                        kernel_size = 32,
                        strides = 2,
                        padding = 'same',
                        activation = 'relu')(c3)
-    c5 = layers.Conv1D(filters = 32,
+    c5 = layers.Conv1D(filters = 32 * SCALE,
                        kernel_size = 32,
                        strides = 2,
                        padding = 'same',
                        activation = 'relu')(c4)
-    dc1 = layers.Conv1DTranspose(filters = 32,
+    dc1 = layers.Conv1DTranspose(filters = 32 * SCALE,
                                  kernel_size = 32,
                                  strides = 1,
                                  padding = 'same')(c5)
     conc = layers.Concatenate()([c5,dc1])
-    dc2 = layers.Conv1DTranspose(filters = 16,
+    dc2 = layers.Conv1DTranspose(filters = 16 * SCALE,
                                  kernel_size = 32,
                                  strides = 2,
                                  padding = 'same')(conc)
     conc = layers.Concatenate()([c4,dc2])
-    dc3 = layers.Conv1DTranspose(filters = 8,
+    dc3 = layers.Conv1DTranspose(filters = 8 * SCALE,
                                  kernel_size = 32,
                                  strides = 2,
                                  padding = 'same')(conc)
     conc = layers.Concatenate()([c3,dc3])
-    dc4 = layers.Conv1DTranspose(filters = 4,
+    dc4 = layers.Conv1DTranspose(filters = 4 * SCALE,
                                  kernel_size = 32,
                                  strides = 2,
                                  padding = 'same')(conc)
     conc = layers.Concatenate()([c2,dc4])
-    dc5 = layers.Conv1DTranspose(filters = 2,
+    dc5 = layers.Conv1DTranspose(filters = 2 * SCALE,
                                  kernel_size = 32,
                                  strides = 2,
                                  padding = 'same')(conc)
     conc = layers.Concatenate()([c1,dc5])
-    dc6 = layers.Conv1DTranspose(filters =1,
+    dc6 = layers.Conv1DTranspose(filters = 1,
                                  kernel_size = 32,
                                  strides = 2,
                                  padding = 'same')(conc)
@@ -133,7 +163,7 @@ def build_model(input_size = 64000):
     return model
 model = build_model(input_size = batching_size)
 #model.summary()
-model.load_weights(models.load_model("./experiment/20210222/NoiseSuppressionModel.h5"))
+# model.load_weights("./experiment/20210222-185036/NoiseSuppressionModel.h5")
 
 ################################################################################
 ## create folder
@@ -147,7 +177,7 @@ if not os.path.exists(train_dir):
 ## training model
 ################################################################################
 
-from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.callbacks import LearningRateScheduler, TensorBoard
 def learning_rate(step_size, decay, verbose = 1):
     def schedule(epoch, lr):
         if epoch > 0 and epoch % step_size == 0:
@@ -159,26 +189,27 @@ def learning_rate(step_size, decay, verbose = 1):
 
 def tensor_board(path):
     return TensorBoard(log_dir = os.path.join(path, 'log'), 
-                       histogram_freq = 0,
+                       histogram_freq = 1,
                        write_graph = True,
-                       write_images = True,
+                       write_images = False,
                        update_freq = 5000, ## 'epoch' for save every epoch 
                        # profile_batch = 0,  ## 2 for enable
                        embeddings_freq = 0, ## 1 for enable 
                        )
 
 callbacks = [
-    learning_rate(step_size = 50, decay = 0.9),
+    learning_rate(step_size = 100, decay = 0.9),
     tensor_board(train_dir),
 ]
-
-model.compile(optimizer=tf.keras.optimizers.Adam(0.002),
+# lr = 2e-3
+lr = 3e-3
+model.compile(optimizer=tf.keras.optimizers.Adam(lr),
               loss=tf.keras.losses.MeanAbsoluteError())
 history = model.fit(train_dataset,
-                    epochs = 500,
+                    epochs = 1000,
                     batch_size = 16, 
                     validation_data = test_dataset,
-                    validation_steps = 5078,
+                    validation_steps = test_dataset.__len__(),
                     callbacks = callbacks,
                     shuffle = True, 
                     )
@@ -231,8 +262,6 @@ converted_model = build_model(input_size = 4410)
 converted_model.set_weights(model.get_weights())
 mlmodel = coremltools.convert(converted_model)
 mlmodel.save(os.path.join(train_dir, "NoiseSuppressionModel.mlmodel"))
-
-
 
 
 
